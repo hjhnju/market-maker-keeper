3c0ce087c753e0fd5a0456da3c2777e649f544be
hjhnju <hjh.nju@gmail.com>
2019-02-19 23:22:32 +0800

    new percent for stategy

--- futures_maker/strategy.py
+++ futures_maker/strategy.py
@@ -108,16 +108,16 @@ class TrandStrategy(Strategy):
                           f"is_enter_long:{self.is_enter_long}, is_enter_short:{self.is_enter_short}")
 
         if self.is_enter_long is False and \
-                self.spot_candle60s_last['percent'] >= Wad.from_number(0.002) and \
-                self.spot_candle60s_last['volume'] >= Wad.from_number(1000):
+                self.spot_candle60s_last['percent'] >= Wad.from_number(0.003) and \
+                self.spot_candle60s_last['volume'] >= Wad.from_number(2000):
             enter_price = self.swap_ticker_last['best_ask']
             self.logger.info(f"Match enter long. percent:{self.spot_candle60s_last['percent']}, volume: {self.spot_candle60s_last['volume']}, "
                              f"enter_price:{enter_price}, enter_size:{enter_size}")
             return Strategy.ENTER_LONG, enter_price, enter_size
 
         if not self.is_enter_short and \
-                self.spot_candle60s_last['percent'] <= Wad.from_number(-0.002) and \
-                self.spot_candle60s_last['volume'] >= Wad.from_number(1000):
+                self.spot_candle60s_last['percent'] <= Wad.from_number(-0.003) and \
+                self.spot_candle60s_last['volume'] >= Wad.from_number(2000):
             enter_price = self.swap_ticker_last['best_bid']
             self.logger.info(f"Match enter short. percent:{self.spot_candle60s_last['percent']}, volume: {self.spot_candle60s_last['volume']}, "
                              f"enter_price:{enter_price}, enter_size:{enter_size}")
@@ -151,7 +151,8 @@ class TrandStrategy(Strategy):
 
             self.logger.debug(f"Check if match exit short. gap_price_percent:{gap_price_percent}, gap_time:{gap_time.seconds}, best_ask:{exit_price}")
             if gap_price_percent >= 1.0 or (gap_price_percent >= 0.05 and gap_time.seconds >= 60) or (gap_time.seconds >= 3600):
-                self.logger.info(f"Match exit long. gap_price_percent:{gap_price_percent}, gap_time:{gap_time.seconds}, exit_price:{exit_price}")
+                self.logger.info(f"Match exit short. gap_price_percent:{gap_price_percent}, gap_time:{gap_time.seconds}, "
+                                 f"enter_price:{enter_price}, exit_price:{exit_price}")
                 return Strategy.EXIT_SHORT, exit_price, exit_size
 
         # (exit_long_or_short, exit_price, exit_size)
